84f403e7bd7cd5fb06957c249c8609fc66548b62
Ryan Hampton <hamptonryan88@gmail.com>
2023-08-02 13:12:10 -0500

    finished ten functions exercise

--- solutions.js
+++ solutions.js
@@ -21,3 +21,71 @@
  * concat
  */
 
+function isTrue(x){
+    if (x != true) {
+        return false;
+    }
+    return true;
+}
+
+function isFalse(x) {
+    if (x === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function not(x) {
+    if (x == false || x == null || x == isNaN(x) || x !== x) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function addOne(x){
+    if (x === Infinity) {
+        return x + 1;
+    }
+    let y = parseInt(x);
+        return y + 1;
+    }
+
+function isEven(x) {
+    if (x % 2 === 0) {
+        return true;
+    } else if (x === false) {
+        return false;
+    } else {
+        return false;
+    }
+}
+
+function isIdentical(x, y) {
+    if (x === y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isEqual (x, y) {
+    if (x == y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function or (x, y) {
+    return x || y;
+}
+
+function and (x, y) {
+    return x && y;
+}
+
+function concat (arg1, arg2) {
+    return arg1 +""+ arg2;
+}
